a55f109b0b6cdc7a89f94675dd79bacd941706ba
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-05 20:25:54 +0300

    start homework 2

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -21,12 +21,12 @@ export const UserList = (props: UserListPropsType) => {
             <h2>User List:</h2>
 
             <ul>
-              {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-                <li key={user.id} id={`hw01-user-${user.id}`}>
-                  <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
-                  {user.address.street}, {user.address.city}
-                </li>
-              ))}
+                {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+                    <li key={user.id} id={`hw01-user-${user.id}`}>
+                        <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                        {user.address.street}, {user.address.city}
+                    </li>
+                ))}
             </ul>
         </div>
     );
